2ab5bcb90206ae63744ff1611358d79a8872d6cf
Pragun Tiwari <praguntiwari1711@gmail.com>
2026-01-31 12:21:33 +0530

    Use DATABASE_URL for Railway MySQL

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "mysql+mysqlconnector://root:password@localhost/marketplace"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
